582419b620e9a0124a2a4bdbd18bd525aececf46
Hiro-824 <thefilepreserver@gmail.com>
2025-07-02 19:45:57 +0900

    Add error (500) and not-found (404) pages.

---
+++ src/app/error.tsx
@@ -0,0 +1,8 @@
+"use client"
+export default function ErrorPage() {
+    return (
+        <div className="flex items-center justify-center min-h-screen p-4">
+            <h1 className="text-2xl text-center">500｜Page not found</h1>
+        </div>
+    );
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return (
+        <div className="flex items-center justify-center min-h-screen p-4">
+            <h1 className="text-2xl text-center">404｜Page not found</h1>
+        </div>
+    );
+}
