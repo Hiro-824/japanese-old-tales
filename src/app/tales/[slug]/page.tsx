13400467b1c910c56a4db0d0a6ea37fd32ddfb54
Hiro-824 <thefilepreserver@gmail.com>
2025-07-02 18:14:46 +0900

    Complete tale page.

--- src/app/tales/[slug]/page.tsx
+++ src/app/tales/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { format, parseISO } from 'date-fns'
 import { allTales } from 'contentlayer/generated'
+import Link from 'next/link'
 
 export const generateStaticParams = async () => allTales.map((tale) => ({ slug: tale._raw.flattenedPath }))
 
@@ -15,10 +16,14 @@ const TaleLayout = ({ params }: { params: { slug: string } }) => {
 
   return (
     <article className="mx-auto max-w-xl py-8">
-      <div className="mb-8 text-center">
-        <time dateTime={tale.date} className="mb-1 text-xs text-gray-600">
-          {format(parseISO(tale.date), 'LLLL d, yyyy')}
-        </time>
+      <div className="mb-4">
+        <Link href="/" className="inline-block text-2xl hover:underline" aria-label="Back to top page">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="currentColor" viewBox="0 0 256 256">
+            <path d="M224,128a8,8,0,0,1-8,8H59.31l58.35,58.34a8,8,0,0,1-11.32,11.32l-72-72a8,8,0,0,1,0-11.32l72-72a8,8,0,0,1,11.32,11.32L59.31,120H216A8,8,0,0,1,224,128Z"></path>
+          </svg>
+        </Link>
+      </div>
+      <div className="mt-8 mb-8 text-left">
         <h1 className="text-3xl font-bold">{tale.title}</h1>
       </div>
       <div className="[&>*]:mb-3 [&>*:last-child]:mb-0" dangerouslySetInnerHTML={{ __html: tale.body.html }} />
