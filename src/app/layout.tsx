76520a258f6317484f42886da347988ab1c3fb1f
Hiro-824 <thefilepreserver@gmail.com>
2025-07-02 17:56:25 +0900

    Set default font to Noto Serif.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Noto_Serif } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const notoSerif = Noto_Serif({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-noto-serif',
+  display: 'swap',
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={notoSerif.variable}>{children}</body>
     </html>
   )
 }
